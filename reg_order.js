147a90fd661f1a5dfb31d83aed74ac3663f1f75d
kasamoke <40295413+kasamoke@users.noreply.github.com>
2020-06-30 22:29:18 +0530

    Add files via upload

--- reg_order.js
+++ reg_order.js
@@ -2,7 +2,6 @@ const { query } = require('express');
 
 module.exports = (app,con) =>{
 
-    //for parsing the body of the POST request
     var bodyParser = require('body-parser');
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,25 +13,23 @@ module.exports = (app,con) =>{
         //this will return all the orders for a perticular user(id)
         if(operation === 'getallorders')
         {
-            res.redirect(303,'/getallOrders');
+             res.redirect(307,'/getallOrders');
         }
         else{res.json({"err_message":"key value error"}); }
         
     });
 
-    app.get('/getallOrders',(req,res)=>{
+    app.post('/getallOrders',(req,res)=>{
          var userId = req.body.userId;
-        // var querys = "select * from reg_order_dat where userId = '" + userId + "'";
+         var querys = "select * from reg_order_dat where userId = '" + userId + "'";
 
-         res.json({"userid":userId});
-
-        // con.query(querys,(err,result)=>{
-        //     if(err){
-        //         res.json({"err_message":"Some query error occurred"}); 
-        //     }
-        //     else{
-        //         res.json(JSON.stringify(result));
-        //     }
-        // });
+        con.query(querys,(err,result)=>{
+            if(err){
+                res.json({"err_message":"Some query error occurred"}); 
+            }
+            else{
+                res.json(JSON.stringify(result));
+            }
+        });
     });
-}
+} 
