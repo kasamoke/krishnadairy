908392c87dde67709fb83ab3ac1450499f977ddb
kasamoke <40295413+kasamoke@users.noreply.github.com>
2020-07-21 20:28:52 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 
 var getConnection = require('./dbconnection');
-require('./user')(app, getConnection);
-require('./reg_order')(app, getConnection);
+require('./user')(app,getConnection);
+require('./reg_order')(app,getConnection);
+require('./rep_order')(app,getConnection);
 
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
